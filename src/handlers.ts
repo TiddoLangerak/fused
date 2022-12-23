d65e1c8b5c243c5363ebf28ccc139c9b52151468
Tiddo Langerak <tiddolangerak@gmail.com>
2022-12-23 16:27:20 +0200

    Add handler utils

--- src/handlers.ts
+++ src/handlers.ts
@@ -1,12 +1,16 @@
 /// For whatever reason the reference is needed for the language-server
 /// <reference path="../types/fuse-native.d.ts" />
-import Fuse, { CB, Handlers } from "fuse-native";
+import Fuse, { CB, Handlers, Stat } from "fuse-native";
 import { debug } from "./debug.js";
 import { FusedFs } from "./fusedFs.js";
 import { ProgramOpts } from "./opts.js";
 import { resolver } from './path.js';
-import { VirtualFs } from "./virtualFs.js";
+import { VirtualFs } from "./virtualfs/index.js";
 import * as fs from 'node:fs/promises';
+import { Awaitable } from "./awaitable.js";
+export { Stat };
+
+// TODO clean up some
 
 function handleError(e: any): number {
   if (e.errno) {
@@ -19,7 +23,7 @@ function handleError(e: any): number {
 }
 
 // Handles mapping a promise-returning function to a c-style cb.
-function $<T>(cb: CB<T>, fn: () => Promise<T>) {
+function $<T>(cb: CB<T>, fn: () => Awaitable<T>) {
   (async() => {
     try {
       const res = await fn();
@@ -29,6 +33,111 @@ function $<T>(cb: CB<T>, fn: () => Promise<T>) {
     }
   })();
 }
+
+
+type ToAwaitable<F> = F extends (cb: CB<infer R>) => void
+? () => Awaitable<R>
+: F extends (a: infer A, cb: CB<infer R>) => void
+? (a: A) => Awaitable<R>
+: F extends (a: infer A, b: infer B, cb: CB<infer R>) => void
+? (a: A, b: B) => Awaitable<R>
+: F extends (a: infer A, b: infer B, c: infer C, cb: CB<infer R>) => void
+? (a: A, b: B, c: C) => Awaitable<R>
+: never;
+
+// Read and write have a different signature
+type StandardHandlers = Exclude<keyof Handlers, 'read' | 'write'>;
+
+export type FusedHandlers = {
+  [k in StandardHandlers]: ToAwaitable<Handlers[k]>;
+} & {
+  write(path: string, fd: number, buffer: Buffer, length: number, position: number): Awaitable<number>;
+  read(path: string, fd: number, buffer: Buffer, length: number, position: number): Awaitable<number>;
+}
+
+function fusedHandlerToNativeHandler<K extends StandardHandlers>(h: Partial<Handlers>, f: Partial<FusedHandlers>, k: K) {
+  const func: FusedHandlers[K] | undefined = f[k];
+  if (func) {
+    h[k] = (...args) => {
+      const cb = args.pop();
+      debug(k, ...args);
+      // Sorry, can't type it, need to do some funky stuff.
+      // Essentially we're just passing the args on, except for the cb
+      $(cb, () => func.apply(f, args));
+    };
+  }
+}
+
+const standardHandlers: StandardHandlers[] = [
+    'init',
+    'access',
+    'getattr',
+    'fgetattr',
+    'flush',
+    'fsync',
+    'fsyncdir',
+    'readdir',
+    'truncate',
+    'ftruncate',
+    'readlink',
+    'chown',
+    'chmod',
+    'mknod',
+    'open',
+    'opendir',
+    'release',
+    'releasedir',
+    'create',
+    'utimens',
+    'unlink',
+    'rename',
+    'symlink',
+    'link',
+    'mkdir',
+    'rmdir',
+];
+
+function mapHandlers(f: Partial<FusedHandlers>): Partial<Handlers> {
+  const handlers: Partial<Handlers> = {
+    init(cb) {
+      debug("init");
+      cb(0);
+    }
+  };
+
+  // TODO: maybe interject debug here? And move it out of elsewhere?
+  standardHandlers.forEach(method => fusedHandlerToNativeHandler(handlers, f, method));
+  if (f.read) {
+    // !!! read cb doesn't take an error
+    handlers.read = async (path, fd, buffer, length, position, cb) => {
+      debug('read');
+      try {
+        const bytesRead = await f.read!(path, fd, buffer, length, position);
+        cb(bytesRead);
+      } catch (e) {
+        console.error("Read failed", e);
+        cb(0);
+      }
+    }
+  }
+
+  if (f.write) {
+    // !!! write cb doesn't take an error
+    handlers.write = async (path, fd, buffer, length, position, cb) => {
+      debug('write');
+      try {
+        const bytesWritten = await f.write!(path, fd, buffer, length, position);
+        cb(bytesWritten);
+      } catch (e) {
+        console.error("Write failed", e);
+        cb(0);
+      }
+    }
+  }
+
+  return handlers;
+}
+
 
 export const makeHandlers = (opts: ProgramOpts, virtualFs: VirtualFs): Partial<Handlers> => {
   const getAbsolutePath = resolver(opts);
@@ -43,6 +152,7 @@ export const makeHandlers = (opts: ProgramOpts, virtualFs: VirtualFs): Partial<H
 access: (path, mode, cb) => {
 debug(`access ${path} (mode: ${mode})`);
             // TODO: do we need to do something here?
+            // Nope, not needed, we have defaultAccess true
             cb(0);
             },
             */
@@ -133,7 +243,6 @@ msg: `File not open. FD: ${fd}. Path: ${path}`
       // TODO: existance checking?
       $(cb, () => fs.writeFile(getAbsolutePath(path), Buffer.alloc(0), { mode }));
     },
-    //mknod TODO: not sure how to implement this, or if it's needed.
     //setxattr TODO: only osx, no native node support
     //getxattr TODO: only osx, no native node support
     //listxattr TODO: only osx, no native node support
@@ -188,7 +297,7 @@ msg: `File not open. FD: ${fd}. Path: ${path}`
     },
     releasedir: (path, fd, cb) => {
       debug(`releasedir ${path} (fd: ${fd})`);
-      $(cb, () => fusedFs.closeDir(fd));
+      $(cb, () => fusedFs.releasedir(fd));
     },
     /*
      * TODO: _something_ is wrong with this implementation, but we don't really need it anyway.
