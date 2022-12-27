fc78b44541f833ab9235181a480a00274252112b
Tiddo Langerak <tiddolangerak@gmail.com>
2022-12-27 13:27:12 +0200

    Fix all the tests

--- src/virtualFs.spec.ts
+++ src/virtualFs.spec.ts
@@ -2,17 +2,12 @@ import { VirtualFs, InMemoryFileHandler } from "./virtualfs/index.js";
 
 describe('virtualFs', () => {
   describe('list', () => {
-    // TODO
-    /*
-    const fs = new VirtualFs();
-    fs.registerHandler(new InMemoryFileHandler("/foo/bar", "content"));
-    fs.registerHandler(new InMemoryFileHandler("/foo/baz", "content"));
+    const fs = new VirtualFs(new InMemoryFileHandler('/foo/bar', 'content'), 1, 1);
     it('lists files returned by its handlers', async () => {
-      expect(await fs.readdir("/foo")).toEqual(["bar", "baz"]);
+      expect(await fs.readdir("/foo")).toEqual(["bar"]);
     });
     it(`doesn't list files in other folders`, async () => {
       expect(await fs.readdir("/bar")).toEqual([]);
     });
-    */
   });
 });
